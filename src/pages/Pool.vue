bff7cead2d4be70621af19745dbabe66b63c8937
hzg <1399146376@qq.com>
2020-11-18 18:02:11 +0800

    初步pool页面

--- src/pages/Pool.vue
+++ src/pages/Pool.vue
@@ -1,6 +1,19 @@
 <template>
-<div>
-    <h1>pool页面</h1>
+<div class="pool"> 
+    <div class="pool-top"><h1>Pool</h1></div>
+    <div class="pool-box">
+        <div class="pool-box1">
+            <p>Liquidity provider rewards</p>
+            <p>Liquidity providers earn a 0.3% fee on all trades proportional to their share of 
+               <br>
+               pool. Fees are added to the pool, accrue in real time and can be claimed 
+               <br>
+               withdrawing your liquidity.</p>
+            <p>Read more about providing liquidity</p>
+        </div>
+        <div class="pool-box2"></div>
+    </div>
+    
 </div>
 
 </template>
@@ -12,5 +25,60 @@ export default {
 </script>
 
 <style>
+.pool{
+  height: 1008px;
+  background: #0F1522;
+  /* position: relative; */
+}
+.pool-top{
+  box-sizing: content-box;
+  padding-top: 132px;
+  width: 52px;
+  height: 27px;
+  margin: 0 auto;
+  overflow: hidden;
+  color: #ffffff;
+  font-size: 24px;
+  margin-bottom: 9px;
+}
+.pool-box{
+   
+  margin: 0 auto;
+  width: 720px;
+  height: 506px;
+  background: #171F30;
+  border-radius: 20px;    
+}
+.pool-box1{
+  width: 723px;
+  height: 233px;
+  background: #0D1429;
+  opacity: 0.8203;
+}
+.pool-box1 p:nth-child(1){ 
+    box-sizing: content-box;
+    padding-top: 55px; 
+    margin-left: 34px;
+    font-size: 20px;
+    font-family: ArialMT;
+    color: #EEEEF0;
+    line-height: 23px;
+}
+.pool-box1 p:nth-child(2){
+    margin-top: 12px;
+    margin-left: 34px;
+    font-size: 18px;
+    font-family: ArialMT;
+    color: #EEEEF0;
+    line-height: 21px;
+}
+.pool-box1 p:nth-child(3){
+    margin-top: 24px;  
+    font-size: 18px;
+    margin-left: 34px;
+    font-family: ArialMT;
+    color: #878B97;
+    line-height: 21px;
+}
 
 </style>
