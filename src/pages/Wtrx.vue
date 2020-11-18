8d803817fe230ffb8b20a62a4b07623cd1da8151
hzg <1399146376@qq.com>
2020-11-18 17:06:04 +0800

    初步wtrx页面

--- src/pages/Wtrx.vue
+++ src/pages/Wtrx.vue
@@ -7,16 +7,16 @@
   <div class="wtrx-left">
     <p>TRX->WTRX</p>
     <p>TRX Balance:</p>
-    <input type="text">
+    <input type="text" placeholder="Please enter the amout of TRX">
     <p>You will get WTRX:</p>
-    <button></button>
+    <button>Confim</button>
   </div>
   <div class="wtrx-right">
     <p>WTRX->TRX</p>
     <p>WTRX Balance:</p>
-    <input type="text">
+    <input type="text" placeholder="Please enter the amout of TRX">
     <p>You will get WTRX:</p>
-    <button></button>
+    <button>Confim</button>
   </div>
   <div class="wtrx-bottom">
     <p>what is WTRX?</p>
@@ -64,21 +64,41 @@ export default {
   color:#ffffff;
 }
 .wtrx-left p:nth-child(1){
+  margin-top: 40px;
   text-align: center;
   color:#ffffff;
 }
 .wtrx-left p:nth-child(2){
+  margin-top: 40px;
   text-align: center;
   color:#ffffff;
 }
+
 .wtrx-left input{
-  overflow: hidden;
-  margin-left: 120px;
+  margin-top: 12px;
+  font-size: 16px;
+  font-family: ArialMT;
+  color: #878B97;
+  line-height: 18px;
+  width: 320px;
+  height: 48px;
+  background: #0F1522;
+  border-radius: 24px;
+  margin-left: 40px;
+}
+.wtrx-left p:nth-child(4){
+  margin-top: 40px;
 }
 .wtrx-left button{
-  width: 150px;
-  height: 20px;
-  margin-left: 120px;
+  margin-top: 12px;
+  width: 320px;
+  height: 48px;
+  background: linear-gradient(136deg, #FDAB2B 0%, #DF0019 100%);
+  border-radius: 24px;
+  margin-left: 40px;
+  color: #ffffff;
+  font-size: 18px;
+  line-height: 21px;
 }
 .wtrx-right p{
   text-align: center;
@@ -93,13 +113,43 @@ export default {
   border-radius: 20px;
   background: #171F30;
 }
-.wtrx-right P:nth-child(1){
+.wtrx-right p:nth-child(1){
+  margin-top: 40px;
   text-align: center;
   color:#ffffff;
 }
-.wtrx-right P:nth-child(2){
+.wtrx-right p:nth-child(2){
+  margin-top: 40px;
   text-align: center;
   color:#ffffff;
+}
+
+.wtrx-right input{
+  font-size: 16px;
+  font-family: ArialMT;
+  color: #878B97;
+  line-height: 18px;
+  margin-top: 12px;
+  width: 320px;
+  height: 48px;
+  background: #0F1522;
+  border-radius: 24px;
+  margin-left: 40px;
+}
+.wtrx-right p:nth-child(4){
+  margin-top: 40px;
+}
+
+.wtrx-right button{
+  margin-top: 12px;
+  margin-left: 40px;
+  width: 320px;
+  height: 48px;
+  background: linear-gradient(136deg, #FDAB2B 0%, #DF0019 100%);
+  border-radius: 24px;
+  color: #ffffff;
+  font-size: 18px;
+  line-height: 21px;
 }
 
 
@@ -120,20 +170,22 @@ export default {
   line-height: 21px;
 }
 .wtrx-bottom p:nth-child(2){
+  margin-top: 8px;
   font-weight: bold;
   font-size: 18px;
   color: #A6AEB7;
   text-align: center;
   line-height: 24px; 
   font-family: Arial-BoldMT, Arial;
- font-weight: normal;
+  font-weight: normal;
 }
-.wtrx-bottom p:nth-child(3){
-  
+.wtrx-bottom p:nth-child(3){  
+  margin-top: 20px;
   color: #ffffff;
   text-align: center;  
 }
 .wtrx-bottom p:nth-child(4){
+  margin-top: 8px;
   font-weight: bold;
   color: #A6AEB7;
   text-align: center;  
