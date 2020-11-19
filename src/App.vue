9e46be17c5cc575bf61ecde90b1b3f746392fd22
hzg <1399146376@qq.com>
2020-11-19 10:27:44 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -22,37 +22,5 @@ html, body, #app{
   background: #0F1522; 
 }
 
-.nav {
-  /* width: 1920px; */
-  line-height: 72px;
-  height: 72px;
-  background-color:#131A28;
-  
-}
-.logo{
-  float: left;
-  width: 35px;
-  height: 35px;
-  margin-top: 10px;
-  margin-left: 33px;
-}
-.logo img{
-  width: 100%;
-}
-.nav-header{
-  
-  color: #B7BFC8;
-}
-.nav-header span {
-  font-size: 22px;
-  margin: 0 26px;
-  cursor: pointer;
-  /* color: #B7BFC8; */
-  /* text-align: center; */
-}
-.on {
-  color:#ffffff;
-  
-}
 
 </style>
