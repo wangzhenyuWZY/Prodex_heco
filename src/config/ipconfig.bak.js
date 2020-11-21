325f64b6efa07ebd79111dc7b83f46d94cc62203
hzg <1399146376@qq.com>
2020-11-21 20:42:36 +0800

    token

--- src/config/ipconfig.bak.js
+++ src/config/ipconfig.bak.js
@@ -4,6 +4,7 @@
 const ipConfig = {
   wtrxAddress: 'TGky6zKfhrisXh92emTvPAbkenhXHTU8m6',
   BFactory:'TYfVssWwJRFaHM9iHqRdG4Z3HWbXhkC78N',
+  MasterChef:'THLRMMtPY6PUnsRKAbJ68bj99Q9RD6t9Wk',
   
 }
 export default ipConfig
