32c31fbd369d44c9765f52086f839062fec2a75d
defiiron@gmail.com <defiiron@gmail.com>
2020-11-23 12:12:56 +0800

    Use docker_access_token

--- sh/build.sh
+++ sh/build.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-ACCESS_TOKEN='7c96ae10-725b-43b5-b163-18e655a65baf'
 
 yarn build
 
 docker build -t  foxdex/web:latest .
 
-docker login --username foxdex -p $ACCESS_TOKEN
+docker login --username foxdex -p $DOCKER_ACCESS_TOKEN
 
 
 docker push foxdex/web:latest
