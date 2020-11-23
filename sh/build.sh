10c824d14260b50f93a9554ab6e5c894806d3a60
defiiron@gmail.com <defiiron@gmail.com>
2020-11-23 15:17:45 +0800

    Add env

--- sh/build.sh
+++ sh/build.sh
@@ -8,10 +8,10 @@ yarn build
 docker build -t  foxdex/web:latest .
 
 echo "DOCKER_ACCESS_TOKEN"
-echo $DOCKER_ACCESS_TOKEN
+echo $env:DOCKER_ACCESS_TOKEN
 echo "END"
 
-docker login --username foxdex -p $DOCKER_ACCESS_TOKEN
+docker login --username foxdex -p $env:DOCKER_ACCESS_TOKEN
 
 
 docker push foxdex/web:latest
