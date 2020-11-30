242d29ae7699554070a7fc423ca42b39319902c8
zhangyang <1105626801@qq.com>
2020-11-30 13:55:30 +0800

    ser farm

--- src/pages/Stake/farmList.js
+++ src/pages/Stake/farmList.js
@@ -2,14 +2,15 @@
   {
     name:'inverst',
     lpToken:'TNFjWx7h4X9LqGcfJumnTsKDdzN1ePvQ5C',
-    perBlockToken: "1950000000000000000"
-    
+    perBlockToken: "1950000000000000000",
+    price:1
     
   },
   {
     name:'Dai Stablecoin',
     lpToken:'TNQPFq9hXJiHBbbVdokPLoMQz87ZUb46sK',
-    perBlockToken: "1107553543102616000"
+    perBlockToken: "1107553543102616000",
+    price:1
   }
 ];
 export default farmlist
