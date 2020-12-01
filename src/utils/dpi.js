3609a10cd953a0ad54b2862fbe32df43448a077e
zhangyang <1105626801@qq.com>
2020-12-01 14:04:18 +0800

    api

--- src/utils/dpi.js
+++ src/utils/dpi.js
@@ -3,8 +3,8 @@
     let meta = document.createElement("meta");
     let scale = window.screen.width /750;
     var str = `width=750, initial-scale=${scale},maximum-scale=${scale},minimum-scale=${scale}, user-scalable=no,target-densitydpi=device-dpi,minimal-ui,uc-fitscreen=no`;
-    var str1 = `width=device-width,initial-scale=1, user-scalable=no,target-densitydpi=device-dpi,minimal-ui,uc-fitscreen=no`;
+    // var str1 = `width=device-width,initial-scale=1, user-scalable=no,target-densitydpi=device-dpi,minimal-ui,uc-fitscreen=no`;
     meta.name = 'viewport'
-    meta.content = str1;
+    meta.content = str;
     head[0].appendChild(meta);
 })()
