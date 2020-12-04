067632055d232ce1ae17bae75979509f8cc0c2ad
defiiron@gmail.com <defiiron@gmail.com>
2020-12-04 16:19:25 +0800

    Use dev

--- sh/build.sh
+++ sh/build.sh
@@ -5,9 +5,9 @@ npm test
 
 yarn build
 
-docker build -t  foxdex/web:latest .
+docker build -t  foxdex/web:dev .
 
 docker login --username foxdex -p $DOCKER_ACCESS_TOKEN
 
 
-docker push foxdex/web:latest
+docker push foxdex/web:dev
