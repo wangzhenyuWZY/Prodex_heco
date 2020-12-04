eb9721877c4b4996b19937874c364b82312ad89e
defiiron@gmail.com <defiiron@gmail.com>
2020-12-04 16:58:33 +0800

    Use dev

--- sh/restart.sh
+++ sh/restart.sh
@@ -2,7 +2,7 @@
 
 docker login --username foxdex -p $DOCKER_ACCESS_TOKEN
 
-docker rmi -f foxdex/web:latest
+docker rmi -f foxdex/web:dev
 docker-compose  -f  docker-compose.yml  pull foxdex
 
 docker-compose -f docker-compose.yml down
