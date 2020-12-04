68ad6d2f5bb235e494315bbf7c66f3aa37119822
wangye <553311122@qq.com>
2020-12-05 00:24:04 +0800

    更改合约

--- src/utils/token.js
+++ src/utils/token.js
@@ -21,7 +21,7 @@ export const tokenList = [
 ]
 export const pairList = [
     {
-        address:'TABdMKUCX81wRA7PRQxXzKBTQEGYZjbeUD',
+        address:'TV6jcZt8QDcDmw3yMgrWYumRNBrrPQhfJq',
         token1:{ 
             name:'WTRX',
             address:'TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR',
