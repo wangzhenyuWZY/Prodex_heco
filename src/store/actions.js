28be82313b74e089936b06bebe01f7c36143e31b
zhangyang <1105626801@qq.com>
2020-12-05 01:32:09 +0800

    add set

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,11 +4,9 @@ import * as types from './action-types';
  const action = {
    async connectWallett ({commit},flag) {
        if (flag === false) return;
-       console.log(flag);
         try {
-            let res= await window.tronWeb.trx.getAccount(window.tronWeb.defaultAddress.base58);
-            console.log(res);
-            commit(types.CONNECT_WALLET,res);
+                let res= await window.tronWeb.trx.getAccount(window.tronWeb.defaultAddress.base58);
+                commit(types.CONNECT_WALLET,res);
         } catch (error) {
             console.log(error)
         }
