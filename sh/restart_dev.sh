3208808e64abea341c9ad05dc08c224824775195
defiiron@gmail.com <defiiron@gmail.com>
2020-12-14 16:43:04 +0800

    Use docker-compose-dev.yml

--- sh/restart_dev.sh
+++ sh/restart_dev.sh
@@ -3,7 +3,7 @@
 docker login --username foxdex -p $DOCKER_ACCESS_TOKEN
 
 docker rmi -f foxdex/web:dev
-docker-compose  -f  docker-compose.yml  pull foxdex
+docker-compose  -f  docker-compose-dev.yml  pull foxdex
 
-docker-compose -f docker-compose.yml down
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose-dev.yml down
+docker-compose -f docker-compose-dev.yml up -d
