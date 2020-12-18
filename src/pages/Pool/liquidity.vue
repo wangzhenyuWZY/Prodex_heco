b684ca0802642e3b743772f1432e747135aeaf71
zhangyang <1105626801@qq.com>
2020-12-18 19:11:21 +0800

    add box

--- src/pages/Pool/liquidity.vue
+++ src/pages/Pool/liquidity.vue
@@ -16,30 +16,21 @@
           <samp class="pool-p">{{$t('pool.Yly')}}</samp>
           <div class="pool-butt1">
             <router-link to="/pool/pairs">
-              <el-button class="from_botton red_button bottun_hei"
-                >{{$t('pool.cj1')}}</el-button
-              >
+              <el-button class="from_botton red_button bottun_hei">{{$t('pool.cj1')}}</el-button>
             </router-link>
           </div>
           <div class="pool-butt2">
             <router-link to="/pool/connectpool">
-              <el-button 
-              
-              class="from_botton bottun_hei1"
-                >{{$t('pool.al')}}</el-button
-              >
+              <el-button class="from_botton bottun_hei1">{{$t('pool.al')}}</el-button>
             </router-link>
           </div>
         </div>
         <div class="pool_boxbg" v-if="!connectFlag">
-          <samp class="pool-p1"
-            >{{$t('pool.cta')}}</samp
-          >
+          <samp class="pool-p1">{{$t('pool.cta')}}</samp>
           <samp class="pool-p2">
             {{$t('pool.dts')}}<samp class="pool-p3">
-            {{$t('pool.imp')}}</samp
-            >
-            </samp>
+              {{$t('pool.imp')}}</samp>
+          </samp>
         </div>
         <div class="fees" v-if="connectFlag">
           <div class="fees_account" v-show="false">
@@ -50,18 +41,14 @@
             <div class="fees_curny clearfix" @click="toggleDrop(item)">
               <div class="curny_lt fl_lt">
                 <span class="lt_img">
-                  <img src="@/assets/img/btc.svg" alt="" />
-                  <img src="@/assets/img/btc.svg" alt="" />
+                  <img :src="requierImg(item.pair,0)" alt="" />
+                  <img :src="requierImg(item.pair,1)" alt="" />
                 </span>
                 <span class="curny_size"> {{item.pair }} </span>
               </div>
               <div class="cyrny_rg fl_rg">
                 <span>Manage</span>
-                <img
-                  src="@/assets/img/icon_down.png"
-                  :class="item.show ? 'actvimg' : 'liveimg'"
-                  alt=""
-                />
+                <img src="@/assets/img/icon_down.png" :class="item.show ? 'actvimg' : 'liveimg'" alt="" />
               </div>
             </div>
 
@@ -71,19 +58,19 @@
                   <div class="received liqui">
                     <div class="lt">
                       <span> {{$t('pool.Ytpt')}} </span>
-                    </div> 
+                    </div>
                     <span class="rg">{{item.myBalanceInPool}}</span>
                   </div>
                   <div class="received liqui  mrgtop16">
                     <div class="lt">
-                      <img src="@/assets/img/btc.svg" alt="" />
+                      <img :src="requierImg(item.pair,0)" alt="" />
                       <span>{{$t('pool.Pooled')}} {{item.token1.name}}:</span>
                     </div>
                     <span class="rg">{{token1Balance.toFixed(6)}}</span>
                   </div>
                   <div class="received  liqui mrgtop16">
                     <div class="lt">
-                      <img class="lt_icon" src="@/assets/img/btc.svg" alt="" />
+                      <img class="lt_icon" :src="requierImg(item.pair,1)" alt="" />
                       <span>{{$t('pool.Pooled')}} {{item.token2.name}}:</span>
                     </div>
                     <span class="rg">{{token2Balance.toFixed(6)}}</span>
@@ -96,18 +83,14 @@
                   </div>
                   <div class="accrued">
                     {{$t('pool.Vafas')}}
-                    <img
-                      class="acrued_img"
-                      src="@/assets/img/icon_jump_green.png"
-                      alt=""
-                    />
+                    <img class="acrued_img" src="@/assets/img/icon_jump_green.png" alt="" />
                   </div>
                   <div class="accrued_btn clearfix">
                     <div class="btn_lt fl_lt">
-                        <el-button class="from_botton" @click="toPool(item)">{{$t('pool.Add')}}</el-button>
+                      <el-button class="from_botton" @click="toPool(item)">{{$t('pool.Add')}}</el-button>
                     </div>
                     <div class="btn_lt fl_rg">
-                        <el-button class="from_botton" @click="toRemove(item)"> {{$t('pool.Remove')}} </el-button>
+                      <el-button class="from_botton" @click="toRemove(item)"> {{$t('pool.Remove')}} </el-button>
                     </div>
                   </div>
                 </div>
@@ -120,8 +103,8 @@
                  <router-link to="/pool/importpool"><samp class="pool-p3">
             Import it. </samp>
             </router-link> -->
-              
-              <!-- </samp
+
+            <!-- </samp
             > -->
           </div>
         </div>
@@ -133,111 +116,128 @@
 <script>
 const Decimal = require('decimal.js');
 import { mapActions, mapState } from "vuex";
-import {TokenData,PairData} from '../../utils/index'
-import {getBalanceInPool,getMyBalanceInPool,getLpBalanceInPool} from "../../utils/tronwebFn"
+import { TokenData, PairData } from '../../utils/index'
+import { getBalanceInPool, getMyBalanceInPool, getLpBalanceInPool } from "../../utils/tronwebFn"
 export default {
   data() {
     return {
       show: false,
-      pairList:[],
-      token1Balance:0,
-      token2Balance:0,
-      myBalanceInPool:0,
-      lpTotal:0,
-      share:0,
-      tokenList:[]
+      pairList: [],
+      token1Balance: 0,
+      token2Balance: 0,
+      myBalanceInPool: 0,
+      lpTotal: 0,
+      share: 0,
+      tokenList: []
     };
   },
   watch: {
-    tokenData(list){
-      this.tokenList = JSON.parse(JSON.stringify(list)) 
+    tokenData(list) {
+      this.tokenList = JSON.parse(JSON.stringify(list))
     },
-    pairData(list){
-      this.pairList = JSON.parse(JSON.stringify(list)) 
-      if(this.pairList && this.pairList.length>0){
+    pairData(list) {
+      this.pairList = JSON.parse(JSON.stringify(list))
+      if (this.pairList && this.pairList.length > 0) {
         this.init()
       }
     }
   },
   computed: {
-    ...mapState(["connectFlag", "walletAddres",'tokenData','pairData']), 
+    ...mapState(["connectFlag", "walletAddres", 'tokenData', 'pairData']),
   },
-  created(){
+  created() {
     this.tokenList = JSON.parse(JSON.stringify(this.tokenData))
     this.pairList = JSON.parse(JSON.stringify(this.pairData))
-    if(this.pairList && this.pairList.length>0){
+    if (this.pairList && this.pairList.length > 0) {
       this.init()
     }
   },
-  mounted(){
-    
+  mounted() {
+
   },
-  methods:{
-    init () {//初始化tronweb
+  methods: {
+    init() {//初始化tronweb
       let that = this
-      this.$initTronWeb().then(function (tronWeb) {
+      this.$initTronWeb().then(function(tronWeb) {
         that.getpairList()
       })
     },
-    toPool(item){
+    toPool(item) {
       this.$router.push({
-          name:"Connectbox",
-          params:{
-            pair:JSON.stringify(item)
-          }
+        name: "Connectbox",
+        params: {
+          pair: JSON.stringify(item)
+        }
       })
     },
-    toRemove(item){
+    toRemove(item) {
       item.token1.balanceInPool = this.token1Balance
       item.token2.balanceInPool = this.token2Balance
-      sessionStorage.setItem('toRemove',JSON.stringify(item));
+      sessionStorage.setItem('toRemove', JSON.stringify(item));
       this.$router.push({
-          name:"removeLiquidity",
-          params:{
-            pair:JSON.stringify(item)
-          }
+        name: "removeLiquidity",
+        params: {
+          pair: JSON.stringify(item)
+        }
       })
     },
-    getpairList(){
+    getpairList() {
       let that = this
-      this.pairList.forEach((item,index)=>{
+      this.pairList.forEach((item, index) => {
         item.show = false
-        getMyBalanceInPool(item).then((res)=>{
-          item.myBalanceInPool = Decimal(res).div(Math.pow(10,18)).toFixed(6).toString()
+        getMyBalanceInPool(item).then((res) => {
+          item.myBalanceInPool = Decimal(res).div(Math.pow(10, 18)).toFixed(6).toString()
           that.$set(that.pairList, index, item)
         })
       })
     },
-    toggleDrop(item){
+    toggleDrop(item) {
       let that = this
-      that.pairList.forEach((ktem)=>{
-        if(ktem.pair!==item.pair){
+      that.pairList.forEach((ktem) => {
+        if (ktem.pair !== item.pair) {
           ktem.show = false
-        }else{
+        } else {
           ktem.show = !item.show
-        } 
+        }
       })
-      
-      if(item.show){
-        getLpBalanceInPool(item).then((res)=>{
-          that.lpTotal = Decimal(res).div(Math.pow(10,18))
-          if(that.lpTotal){
+
+      if (item.show) {
+        getLpBalanceInPool(item).then((res) => {
+          that.lpTotal = Decimal(res).div(Math.pow(10, 18))
+          if (that.lpTotal) {
             that.share = Decimal(item.myBalanceInPool).div(that.lpTotal)
             that.getBalance(item)
           }
         })
-        
+
       }
     },
-    getBalance(item){
+    getBalance(item) {
       let that = this
-      getBalanceInPool(item,item.token1).then((res)=>{
+      getBalanceInPool(item, item.token1).then((res) => {
         that.token1Balance = Decimal(res).mul(that.share)
       })
-      getBalanceInPool(item,item.token2).then((res)=>{
+      getBalanceInPool(item, item.token2).then((res) => {
         that.token2Balance = Decimal(res).mul(that.share)
       })
-    }
+    },
+    requierImg(name, number) {
+      let str;
+      if (name) {
+        try {
+          if (number != undefined) {
+            str = name.split('/');
+            return require('@/assets/img/currency/' + str[number] + '.png')
+
+          }
+          return require('@/assets/img/currency/' + name + '.png')
+        } catch (error) {
+          return require('@/assets/img/currency/avitve.png')
+        }
+      } else {
+        return require('@/assets/img/currency/avitve.png')
+      }
+    },
   }
 };
 </script>
@@ -245,13 +245,13 @@ export default {
 <style lang="scss" scoped>
 .bottun_hei {
   height: 40px;
-  border: 1px solid #0f1730; 
-  color: #070A0E; 
+  border: 1px solid #0f1730;
+  color: #070a0e;
   // font-family: roboto-mediumitalic;
 }
 .bottun_hei1 {
   height: 40px;
-  background: #070A0E;
+  background: #070a0e;
 }
 
 .fees {
@@ -276,10 +276,10 @@ export default {
   }
   .cyrny_bg {
     overflow: hidden;
-    margin-bottom:10px;
+    margin-bottom: 10px;
     background: #f4f5fa;
     border-radius: 0 0 16px 16px;
-    margin-top:15px;
+    margin-top: 15px;
   }
   .fees_curny {
     background: url(../../assets/img/binp.png) no-repeat;
@@ -331,7 +331,7 @@ export default {
     margin-top: 27px;
     .btn_lt {
       width: 190px;
-      button{
+      button {
         height: 48px;
       }
     }
@@ -341,12 +341,18 @@ export default {
     overflow: hidden;
     transition: height 0.3s;
   }
-  .liqui{
-    span{
+  .liqui {
+    display: flex;
+    justify-content: space-between;
+    span {
       font-size: 16px;
     }
-    .lt{
-      color: #878B97;
+    .lt {
+      color: #878b97;
+      img {
+        width: 24px;
+        margin-right: 4px;
+      }
     }
   }
   .actvimg {
@@ -366,31 +372,28 @@ export default {
   }
 }
 @media screen and (max-width: 750px) {
-   .removeLq{
-     margin-top: 0px;
+  .removeLq {
+    margin-top: 0px;
 
-       .pool-box{
-    width: 100%;
-    // padding: 0 0.4rem ;
-  }
-    .pool-box2{
-      min-block-size: inherit; 
+    .pool-box {
+      width: 100%;
+      // padding: 0 0.4rem ;
     }
-    .pool-box1{
-      p:nth-child(2){
+    .pool-box2 {
+      min-block-size: inherit;
+    }
+    .pool-box1 {
+      p:nth-child(2) {
         font-size: 0.4rem;
-     }
-    } 
-   }
-      .fees .fees_share{
-       padding:  0 12px;
-       padding-top: 16px;
-       
-     }
-    .fees .accrued_btn .btn_lt {
-      width: 48%;
-     }
-
+      }
+    }
+  }
+  .fees .fees_share {
+    padding: 0 12px;
+    padding-top: 16px;
+  }
+  .fees .accrued_btn .btn_lt {
+    width: 48%;
+  }
 }
-
 </style>
