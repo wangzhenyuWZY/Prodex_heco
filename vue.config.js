2c58cee91738c317ea609b2d92adebd3a0b46f19
wangye <553311122@qq.com>
2020-12-20 18:03:21 +0800

    打包文件

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const TerserPlugin = require('terser-webpack-plugin');
- const isProduction = process.env.VUE_APP_CURRENTMODE !== 'development';
+//  const isProduction = process.env.VUE_APP_CURRENTMODE !== 'development';
+console.log(process.env.NODE_ENV) 
 module.exports = {
   lintOnSave: false,
   // productionSourceMap:false,
